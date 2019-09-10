18874dd4f2b551aa75d469702d94c29a8f2d19e7
Hyounwoo <hyounoosung@gmail.com>
2019-09-10 14:34:55 +0900

    ROPC

---
+++ ro.Client/Program.cs
@@ -0,0 +1,43 @@
+﻿using IdentityModel.Client;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ro.Client
+{
+    class Program
+    {
+        static async Task Main()
+        {
+            // discover endpoints from metadata
+            var client = new HttpClient();
+
+            var disco = await client.GetDiscoveryDocumentAsync("https://localhost:5001");
+            if (disco.IsError)
+            {
+                Console.WriteLine(disco.Error);
+                return;
+            }
+
+            // request token
+            var tokenResponse = await client.RequestPasswordTokenAsync(new PasswordTokenRequest
+            {
+                Address = disco.TokenEndpoint,
+                ClientId = "ro.client",
+                ClientSecret = "secret",
+
+                UserName = "hyounwoo@endalgo.com",
+                Password = "Roqkf@2",
+                Scope = "endalgoapi"
+            });
+
+            if (tokenResponse.IsError)
+            {
+                Console.WriteLine(tokenResponse.Error);
+                return;
+            }
+
+            Console.WriteLine(tokenResponse.Json);
+        }
+    }
+}
